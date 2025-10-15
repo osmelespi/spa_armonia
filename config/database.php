4e671fdc309b584426e432790cb85d581c2b4577
osmelespi <osmelespinoza84@gmail.com>
2025-10-15 21:36:16 +0200

    comienzo de desarrollo

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database {
+
+    private $host = 'localhost:3307';
+    private $db_name = 'spa_armonia';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+
+?>
